9950a327a576bd1ecc7078df51f2981e2e3a6bbc
Nesegreven <49297130+Nesegreven@users.noreply.github.com>
2023-04-22 19:47:11 +0200

    Update index.js

--- vinlotteri/index.js
+++ vinlotteri/index.js
@@ -35,14 +35,14 @@ var app = new Vue({
                         n = ~~(Math.random() * (this.tickets.length));
                         this.winner = this.tickets[n];
                         this.winningNumber = n;
-                    }, 120000 / i)
+                    }, 150000 / i)
                 }
                 setTimeout(() => {
                     this.previousWinners.push(this.winner);
                     this.removeTicket(this.winningNumber);
                     this.wColor = 'green';
                     this.drawing = false;
-                }, 12050)
+                }, 16050)
             }
         },
         undoDrawing(i) {
